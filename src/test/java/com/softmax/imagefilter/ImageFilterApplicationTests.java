b882e8c3551dc884a1cbb50cf6663e325e2cddca
Suryawanshi <vissurya@bmc.com>
2022-01-07 18:39:20 +0530

    upload lab1 project image filter

---
+++ src/test/java/com/softmax/imagefilter/ImageFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softmax.imagefilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageFilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
